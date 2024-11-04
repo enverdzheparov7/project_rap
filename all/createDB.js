1b6c55d6384e454e635b054f1dbb7c89af971710
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-11-04 22:22:52 +0300

    пункт 6_2

--- all/createDB.js
+++ all/createDB.js
@@ -7,7 +7,7 @@ console.log(data)
 // const url = 'mongodb://localhost:27017';
 // const client = new MongoClient(url);
 // // Database Name
-// const dbName = 'test2024';
+// const dbName = 'rap_2024';
 // async function main() {
 // // Use connect method to connect to the server
 // await client.connect();

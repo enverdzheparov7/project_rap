84dc5f197a6bf6b56b061f3a043444efdd6d82d0
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-11-06 23:58:57 +0300

    пункт 7_3

---
+++ all/models/rap.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+var rapperSchema = new Schema({
+   title: String,
+nick: {
+   type: String,
+   unique: true,
+   required: true
+ },
+   avatar: String,
+   desc: String,
+created:{
+   type:Date,
+   default:Date.now
+ }
+ })
+
+ module.exports.Rapper = mongoose.model("Rapper", rapperSchema)
--- all/testMongoose.js
+++ all/testMongoose.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-var schema = mongoose.Schema({ name: String })
 
-schema.methods.yo = function(){
-    console.log(this.name + " сказал йоу")
-    }
+var Rapper = require('./models/rap.js').Rapper
 
-const Rapper = mongoose.model('Rapper', schema);
-
-const rapper = new Rapper({ name: 'Oxxxymiron' });
-rapper.save().then(() => rapper.yo()); 
+var rapper = new Rapper({
+    title: "Эминем",
+    nick : "Eminem",
+})
+ 
+rapper.save();

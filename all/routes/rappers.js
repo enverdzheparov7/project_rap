0cdb16e53d0b65ad815d05ac900ffc749c0ac2c0
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-11-09 23:04:23 +0300

    пункт 8_3

--- all/routes/rappers.js
+++ all/routes/rappers.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var Rap = require('../models/rap').Rap;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с rappers');
 });
-router.get("/:nick", function(req, res, next) {
-  res.send(req.params.nick);
+router.get("/:nick", async function(req, res, next) {
+  var rap = await Rap.find({nick: req.params.nick});
+  console.log(rap)
+  if(!rap.length) return next(new Error("Нет такого рэпера"))
+  var raps = rap[0];
+  res.render('raps', {
+  title: raps.title,
+  picture: raps.avatar,
+  desc: raps.desc
+  })
   });
+  
   
 module.exports = router;

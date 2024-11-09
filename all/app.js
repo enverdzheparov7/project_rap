30fd6092b5f823abb9d7554e1937a0d6f3d862ce
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-11-09 23:10:03 +0300

    пункт 5

--- all/app.js
+++ all/app.js
@@ -6,10 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { title } = require('process');
 
 var app = express();
 
 // view engine setup
+app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -35,7 +37,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {title:'Rappers'});
 });
 
 module.exports = app;

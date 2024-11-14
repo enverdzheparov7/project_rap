a3bce2402f0aa01c65072bbd52f67d4bc64c6ea6
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-11-14 09:07:22 +0300

    Пункт 10_1

--- all/routes/index.js
+++ all/routes/index.js
@@ -1,3 +1,5 @@
+
+
 var express = require('express');
 var router = express.Router();
 
@@ -7,6 +9,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', counter:req.session.counter });
   });
   
-
+/* GET login/registration page. */
+router.get('/logreg', function(req, res, next) {
+  res.render('logreg',{title: 'Вход'});
+  });
+  
 
 module.exports = router;

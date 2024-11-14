e6b0c6ee4104777a28cddf378395b75ae9eca99f
enverdzheparov7 <dzheparovei.23@gmail.com>
2024-11-14 08:13:20 +0300

    Пункт 9_4

--- all/routes/index.js
+++ all/routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  req.session.greeting = "Hi!!!";
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', counter:req.session.counter });
   });
   
 
